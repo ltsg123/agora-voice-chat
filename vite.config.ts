7d5e9574cfe294ad13521d762c7a4bb9c64dbbbc
ltsg <xiaoshumin@agora.io>
2026-02-04 18:41:50 +0800

    chore: add workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: ltsg xiaoshumin@agora.io
  * @Date: 2024-07-16 16:59:35
  * @LastEditors: ltsg xiaoshumin@agora.io
- * @LastEditTime: 2026-02-04 10:15:44
+ * @LastEditTime: 2026-02-04 18:40:29
  * @FilePath: /agora-voice-chat/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,11 +11,12 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  server: {
-    https: {
-      key: "/Users/config/key.pem",
-      cert: "/Users/config/cert.pem",
-    },
-  },
+  // server: {
+  //   https: {
+  //     key: "/Users/config/key.pem",
+  //     cert: "/Users/config/cert.pem",
+  //   },
+  // },
 });
